845afd6fec03be42c0584c14c26381697f050f64
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2023-12-05 01:21:52 +0100

    refactor: Altera as rotas de categoria para uma única rota resource

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,4 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 */
-Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
-Route::put('categories/{id}', [CategoryController::class, 'update'])->name('categories.update');
-Route::delete('categories/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+Route::apiResource('categories', CategoryController::class);
