965da0229774cc08bf5d0b1ce3f23ff6fa81854a
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2023-12-08 07:48:50 +0100

    fix: Corrige o texto description

--- database/migrations/2023_12_08_072720_create_products_table.php
+++ database/migrations/2023_12_08_072720_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100)->unique();
-            $table->string('descrption')->nullable();
+            $table->string('description')->nullable();
             $table->string('image')->nullable();
             $table->timestamps();
         });
