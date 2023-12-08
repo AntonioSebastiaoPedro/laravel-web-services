27ddcc8681bdf9dcdce8f9e0fb46393a66cf5e2e
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2023-12-08 08:39:28 +0100

    Insere registos de categoria e productos. Com relacionamento entre eles

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -13,9 +14,11 @@ class ProductFactory extends Factory
     protected $model = Product::class;
     public function definition(): array
     {
+        $categories = Category::get('id');
         return [
             'name' => $this->faker->unique()->name,
             'description' => $this->faker->sentence,
+            'category_id' => $categories[array_rand($categories->toArray())]
         ];
     }
 }
--- database/migrations/2023_12_08_072720_create_products_table.php
+++ database/migrations/2023_12_08_072720_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')
+                    ->references('id')->on('categories')
+                    ->cascadeOnDelete();
             $table->string('name', 100)->unique();
             $table->string('description')->nullable();
             $table->string('image')->nullable();
