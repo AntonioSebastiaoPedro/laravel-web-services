79a539f09a56f262d1741d8a97c2701d74a62d82
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2023-12-12 20:13:57 +0100

    feat: Lista produtos com os dados das respectivas categorias

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -43,7 +43,7 @@ class ProductController extends Controller
     
     public function show(string $id)
     {
-        if(!$product = $this->product->find($id)){
+        if(!$product = $this->product->with('category')->find($id)){
             return response()->json(['error' => 'Not Found'], 404);
         }
         return response()->json($product);
